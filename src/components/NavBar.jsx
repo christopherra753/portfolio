79a3a8ba80605eee299aac9ccfc2b8a47cc6cecb
christopher <christopher.romero.753@gmail.com>
2024-01-19 16:42:39 -0500

    Agregando colores

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -22,7 +22,7 @@ function NavBar () {
     <nav className='h-20 p-5 fixed w-full top-0 z-10 bg-white/80 dark:bg-neutral-800/80 backdrop-blur-md'>
       <div className='max-w-5xl h-full mx-auto flex  items-center justify-between'>
         <img className='size-10 rounded-full' src='/hero.jpg' alt='' />
-        <ul className={`z-10 fixed top-0 right-0 bg-gray-100 w-1/2 h-screen flex flex-col items-center justify-center gap-5 transition-all ${menu ? 'translate-x-0' : 'translate-x-full'} md:static md:h-auto md:w-auto md:bg-transparent md:flex-row md:translate-x-0`}>
+        <ul className={`z-10 fixed top-0 right-0 bg-gray-100 dark:bg-neutral-900 p-5 md:p-0 w-1/2 h-screen flex flex-col items-center justify-center gap-5 transition-all ${menu ? 'translate-x-0' : 'translate-x-full'} md:static md:h-auto md:w-auto md:bg-transparent md:flex-row md:translate-x-0`}>
           <NavBarLink setMenu={setMenu} to='inicio' label='Inicio' />
           <NavBarLink setMenu={setMenu} to='experiencia' label='Experiencia' />
           <NavBarLink setMenu={setMenu} to='proyectos' label='Proyectos' />
@@ -32,20 +32,20 @@ function NavBar () {
           <a
             href='/curriculum.pdf'
             download='curriculum'
-            className='bg-orange-500 text-sm sm:text-base text-white px-4 py-1 rounded'
+            className='bg-orange-500 hover:bg-orange-600 transition-colors text-sm sm:text-base text-white px-4 py-1 rounded'
           >Descargar CV
           </a>
-          <button onClick={handleTheme} className='group relative rounded-lg p-1'>
+          <button onClick={handleTheme} className='group relative rounded-lg p-1 hover:bg-gray-100 dark:hover:bg-neutral-900/40 transition-colors'>
             <RiMoonLine className='block dark:hidden text-3xl' />
             <RiSunLine className='hidden dark:block text-3xl text-white' />
-            <p className='absolute hidden group-hover:block -bottom-4 left-1/2 -translate-x-1/2 text-xs min-w-max pointer-events-none font-semibold'>
+            <p className='absolute opacity-0 group-hover:opacity-100 -bottom-4 left-1/2 -translate-x-1/2 text-xs min-w-max pointer-events-none font-semibold transition-all'>
               <span className='block dark:hidden text-black'>Dark Mode</span>
               <span className='hidden dark:block text-white'>Light Mode</span>
             </p>
           </button>
           <button onClick={() => setMenu(!menu)} className='block md:hidden z-10'>
             <RiMenuLine className={`${menu ? 'hidden' : 'block'} text-3xl dark:text-white`} />
-            <RiCloseLine className={`${menu ? 'block' : 'hidden'} text-3xl`} />
+            <RiCloseLine className={`${menu ? 'block' : 'hidden'} text-3xl dark:text-white hover:text-red-500 dark:hover:text-red-500 transition-colors`} />
           </button>
         </div>
       </div>
--- src/components/NavBarLink.jsx
+++ src/components/NavBarLink.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function NavBarLink ({ to, label, setMenu }) {
   return (
-    <a onClick={() => setMenu(false)} className='block text-first  tracking-wide dark:text-cyan-400 font-semibold rounded-lg transition-colors' href={`#${to}`}>{label}</a>
+    <a onClick={() => setMenu(false)} className='block w-full text-center text-first  tracking-wide dark:text-cyan-400 hover:bg-gray-200 dark:hover:bg-neutral-950 font-semibold rounded-lg transition-colors md:hover:bg-gray-100 md:dark:hover:bg-neutral-900/40 py-1 px-4' href={`#${to}`}>{label}</a>
   )
 }
 
